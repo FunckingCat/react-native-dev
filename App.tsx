31b47bcbf394cea09378deea9b8c9849e8ecba58
David Zh <david99111@mail.ru>
2023-12-03 18:43:02 +0300

    Todo app extended

--- App.tsx
+++ App.tsx
@@ -1,118 +1,134 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Что изменено по сравнению с начальной версией:
+// Добавляения типизация для state и новые поля
+// Добавлена возможность выбирать дату дедлайна тудушки
+// В качестве идентификаторов для записей используется не new Date а UUID что по логике больше подходит
+// Дата создания записи отдельно сохраняется в стейт
+// Изменены иконки
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import uuid from 'react-native-uuid';
+import DateTimePicker, { DateType } from 'react-native-ui-datepicker';
+import {Dimensions} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type UUID = string | number[];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+type Item = {
+  id: UUID,
+  createDate: Date,
+  deadLine?: DateType,
+  text: string
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const TodoApp = () => {
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [todos, setTodos] = useState<Item[]>([]);
+  const [textInputValue, setNewTodo] = useState('');
+  const [dateValue, setDateValue] = useState<DateType>(undefined);
+
+  const addTodo = () => {
+    if (textInputValue !== '') {
+      setTodos([...todos, { id: uuid.v4(), createDate: new Date(), deadLine: dateValue, text: textInputValue }]);
+      setNewTodo('');
+      setDateValue(undefined)
+    }
+  };
+
+  const removeTodo = (id: UUID) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Text style={styles.title}>✅ Todo List</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter a new todo"
+          value={textInputValue}
+          onChangeText={(text) => setNewTodo(text)}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={addTodo}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.container}>
+        <DateTimePicker value={dateValue} onValueChange={(date) => setDateValue(date)}/>
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={todos}
+        renderItem={({ item }) => (
+          <View style={styles.todoItem}>
+            <Text style = {styles.todoItemDescription}>{item.text.length > 18 ? item.text.slice(0, 15) + '...' : item.text}</Text>
+            <Text>{item.deadLine?.toLocaleString().slice(0,10)}</Text>
+            <TouchableOpacity onPress={() => removeTodo(item.id)}>
+              <Text style={styles.removeButton}>🗑️</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-             <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 65,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 0,
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginRight: 10,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  list: {
+    flex: 1,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  todoItemDescription: {
+    width: Dimensions.get('window').width / 2,
+  },
+  removeButton: {
+    color: 'red',
+    fontWeight: 'bold',
   },
 });
 
-export default App;
+export default TodoApp;
